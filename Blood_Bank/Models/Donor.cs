ca1d160f74b191a700abd63babbd4ed16f9af885
Fatma Atef <fatmhshybh514@gmail.com>
2026-04-21 18:43:38 +0200

    Backend

---
+++ Blood_Bank/Models/Donor.cs
@@ -0,0 +1,17 @@
+﻿namespace Blood_Bank.Models
+{
+    public class Donor : User
+    {
+        public string ?BloodType { get; set; }
+        public string ? MedicalHistory { get; set; } 
+        public DateTime LastDonationDate { get; set; }
+        public int TotalDonations { get; set; }
+        public int Points { get; set; }
+        public Reward? Reward { get; set; }
+        public ICollection<Appointment> ?Appointments { get; set; }  
+        public ICollection<Notification> ?Notifications { get; set; }
+
+        
+
+    }
+}
---
+++ Blood_Bank/Models/Reward.cs
@@ -0,0 +1,12 @@
+    namespace Blood_Bank.Models;
+
+    public class Reward
+    {
+        public int Id { get; set; }
+        public int Points { get; set; }
+
+
+        public int DonorId { get; set; }
+        public Donor? Donor { get; set; }
+
+    }
