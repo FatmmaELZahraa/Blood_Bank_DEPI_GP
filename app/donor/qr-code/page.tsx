92b386f43040c4750314052e0891352bf1a2585c
Fatma Atef <fatmhshybh514@gmail.com>
2026-05-27 23:10:51 +0300

    replace

--- app/donor/qr-code/page.tsx
+++ app/donor/qr-code/page.tsx
@@ -299,7 +299,7 @@ export default function QRCodePage() {
       }
 
       try {
-        const response = await fetch("http://bloodlinknetwork2.runasp.net/api/Auth/profile", {
+        const response = await fetch("https://bloodlinknetwork2.runasp.net/api/Auth/profile", {
           headers: {
             "Authorization": `Bearer ${token}`
           }
