6fc8e35235e588bb974ebdf82056b315a28ce961
Fatma Atef <fatmhshybh514@gmail.com>
2026-05-27 22:33:02 +0300

    Deploy Backend

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -15,7 +15,7 @@ export default function LoginPage() {
     setError(""); 
 
     try {
-      const response = await fetch("http://localhost:5004/api/auth/login", { 
+      const response = await fetch("http://bloodlinknetwork2.runasp.net/api/auth/login", { 
         method: "POST",
         headers: {
           "Content-Type": "application/json",
